74020bd92aa91135c73fd10f1de34df7e6ee73af
Gollapalli Vinay <vinaygollapalli5678@gmail.com>
2024-04-18 10:56:13 +0530

    first commit

---
+++ src/components/OutlineComponent.jsx
@@ -0,0 +1,9 @@
+
+
+export default function OutlineComponent(){
+    return(
+        <div>
+            <h1>This is an Outline Component</h1>
+        </div>
+    );
+}
---
+++ src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Form from "./Form";
+import TodoList from "./TodoList";
+import Footer from "./Footer";
+
+export default function Todo(){
+    const [todos, setTodos] = useState([]);
+    const completed = todos.filter((todo)=>todo.done).length
+    const total = todos.length
+
+    return (
+        <div>
+            <Form todos={todos} setTodos={setTodos}/>
+            <TodoList todos={todos} setTodos={setTodos}/>
+            <Footer completed={completed} total={total}/>
+        </div>
+    );
+}
